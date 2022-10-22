8ea0ef7b9922ad00f83a835817527d101bcf23ba
Gentjan Balo - MMS <53909184+ggt19@users.noreply.github.com>
2022-10-22 15:59:05 +0200

    Update app.py

--- python/app.py
+++ python/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello Student 15 and flux qa"
+    return "Hello Student 18 and flux qa"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
