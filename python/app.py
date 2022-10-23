933fdd34d31be97c66601b125b0b2381d9861dff
Gentjan Balo - MMS <53909184+ggt19@users.noreply.github.com>
2022-10-23 22:57:01 +0200

    Update app.py

--- python/app.py
+++ python/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "flux sync qa and prod include helm 1"
+    return "flux sync qa and prod include helm 2"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
